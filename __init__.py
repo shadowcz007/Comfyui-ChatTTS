e850b1bdddcbe4107a93f1814bdf8b84d6466bcd
shadowcz007 <chizhiwei007@163.com>
2024-06-10 22:17:07 +0800

    新增多人播客节点

--- __init__.py
+++ __init__.py
@@ -1,12 +1,14 @@
-from .nodes.chat_tts import ChatTTSNode
+from .nodes.chat_tts import ChatTTSNode,multiPersonPodcast
 
 
 NODE_CLASS_MAPPINGS = {
-    "ChatTTS_": ChatTTSNode
+    "ChatTTS_": ChatTTSNode,
+    "MultiPersonPodcast":multiPersonPodcast
 }
 
 # dict = { "key":value }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "ChatTTS_": "ChatTTS"
+    "ChatTTS_": "ChatTTS",
+    "MultiPersonPodcast":"Multi Person Podcast"
 }
