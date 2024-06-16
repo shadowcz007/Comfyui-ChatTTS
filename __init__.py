ae5f6df3ae727b861310f3ce27f14d93232db077
shadowcz007 <chizhiwei007@163.com>
2024-06-16 11:53:22 +0800

    update

--- __init__.py
+++ __init__.py
@@ -1,11 +1,10 @@
 from .nodes.chat_tts import ChatTTSNode,multiPersonPodcast
-from .nodes.audio_play import AudioPlayNode_TEST,EditMask
+from .nodes.audio_play import AudioPlayNode_TEST
 
 NODE_CLASS_MAPPINGS = {
     "ChatTTS_": ChatTTSNode,
     "MultiPersonPodcast":multiPersonPodcast,
-    "AudioPlayNode_TEST":AudioPlayNode_TEST,
-    "EditMask":EditMask
+    "AudioPlayNode_TEST":AudioPlayNode_TEST
 }
 
 # dict = { "key":value }
